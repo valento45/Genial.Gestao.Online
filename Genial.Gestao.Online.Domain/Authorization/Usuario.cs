95fbe1f843683ac38548957388237a4231de3d8c
Igor Carvalho <iggor1935@hotmail.com>
2024-05-29 00:30:16 -0300

    Ajustes Login e Authentication

---
+++ Genial.Gestao.Online.Domain/Authorization/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genial.Gestao.Online.Domain.Authorization
+{
+    public class Usuario : IdentityUser<int>
+    {
+
+        public int IdUsuario { get; set; }
+        public int IdPessoa { get; set; }
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public bool AutenticaDoisFatores { get; set; }
+        public string Celular { get; set; }
+        public bool EmailVerificado { get; set; }
+        
+
+
+        public Usuario()
+        {
+
+        }
+
+        public bool CheckPassword(string passwordHash)
+        {
+            return this.Senha.Equals(passwordHash);
+        }
+    }
+}
---
+++ Genial.Gestao.Online.Domain/Models/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Genial.Gestao.Online.Securitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genial.Gestao.Online.Domain.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string PasswordHash
+        {
+            get
+            {
+                return Security.Encrypt(Senha);
+            }
+        }
+        public string Message { get; set; }
+
+        public bool LembrarMe { get; set; }
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
