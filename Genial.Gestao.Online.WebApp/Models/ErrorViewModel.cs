bd32b848818b6f7b6c273fdadf7296143957ef7a
Igor Carvalho <iggor1935@hotmail.com>
2024-05-29 00:18:36 -0300

    Add project files.

---
+++ Genial.Gestao.Online.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Genial.Gestao.Online.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
